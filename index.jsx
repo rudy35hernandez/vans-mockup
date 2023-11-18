7dccecb6d15014fa7a3881eaa14d20a29823e812
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-11-17 23:59:39 -0800

    imported server in index js

--- index.jsx
+++ index.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+
+import "./server"
 
 function App() {
   return (
