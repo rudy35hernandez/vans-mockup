0d7c38421ac9ec041a135e0c02a53ac5669f0308
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-11-26 22:51:32 -0800

    added useEffect method to pull api info

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,7 +1,27 @@
 import React from "react"
 
 export default function Vans(){
+    const [vansData, setVansData] = React.useState([])
+
+    React.useEffect(() ={
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVansData(data.vans))
+    }, [])
+
+    const vansEl => vansData.map(el => {
+        return (
+            <div>
+                <h2>{el.name}</h2>
+            </div>
+        )
+    })
+
     return (
-        <h1>Vans go here</h1>
+        <div>
+            <h1>Vans go here</h1>
+
+        </div>
+        
     )
 }
