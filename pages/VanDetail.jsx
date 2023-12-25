234d0e75f659b8db7445e233fe9234d0097c2ce7
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-12-25 01:57:23 -0800

    added fetch request to van detail page to grab van data

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,8 +1,23 @@
 import React from "react"
+import {useParams} from "react-router-dom"
 
 
 export default function VanDetail(){
+
+    const params = useParams()
+    const [data, setData] = React.useState([])
+
+    React.useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => setData(data.vans))
+    }, [params.id])
+
     return (
-        <h1>Van details go here</h1>
+        <div>
+            <h1>Van details go here</h1>
+            <img src={data.imageUrl} alt="picture of van" />
+        </div>
+
     )
 }
