951b70f5c2b1dbcb1b4e2964813d0d41189c0058
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2024-01-04 01:05:34 -0800

    nested reviews to see if it works

--- index.jsx
+++ index.jsx
@@ -21,8 +21,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
