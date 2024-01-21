a9d6ca4da46c90ccbb29b60841084bee39b46e63
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2024-01-21 00:21:49 -0800

    added styling for HostLayout nav

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -4,7 +4,7 @@ import  {Link, Outlet} from 'react-router-dom'
 export default function HostLayout(){
     return(
         <>
-            <nav>
+            <nav className="host-nav">
                 <Link to="/host">Dashboard</Link>
                 <Link to="/host/income">Income</Link>
                 <Link to="/host/reviews">Reviews</Link>
