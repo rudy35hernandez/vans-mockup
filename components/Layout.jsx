f6208672ccd151b63191c45e0a05c95160d77174
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2024-02-04 16:36:21 -0800

    added footer and css

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from "./Header"
+import Footer from "./Footer"
 import { Outlet } from "react-router-dom"
 
 export default function Layout(){
     return (
         <>
             <Header />
-            <Outlet />
+            <main >
+                <Outlet />
+            </main>
+            <Footer />
         </>
     )
 }
