95e8bc7e26a3b1f893b35b3a15f48034664222d2
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2024-02-11 00:20:49 -0800

    fixed layout css

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,12 +5,12 @@ import { Outlet } from "react-router-dom"
 
 export default function Layout(){
     return (
-        <>
+        <div className="site-wrapper">
             <Header />
             <main >
                 <Outlet />
             </main>
             <Footer />
-        </>
+        </div>
     )
 }
