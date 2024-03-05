41b0a254718f2dcee4215eed513c7742dd585344
rudy35hernandez <rudy35hernandez@gmail.com>
2024-03-04 23:18:07 -0800

    added hostVans effect and map to server data to render vans

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,7 +1,48 @@
 import React from "react"
 
 export default function HostVans(){
+
+    const [vans, setVans] = React.useState([])
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans`)
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    },[])
+
+    const hostVansEls = vans.map(van => {
+        return (
+            <Link
+                to={`/host/vans/${van.id}`}
+                key={van.id}
+                className="host-van-link-wrapper"
+            >
+                <div className="host-van-single" key={van.id}>
+                    <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                    <div className="host-van-info">
+                        <h3>{van.name}</h3>
+                        <p>{van.price}/day</p>
+                    </div>
+                </div>
+            </Link>
+        )
+    })
+
     return (
-        <h1>This is the host Vans page</h1>
+        <section>
+            <h1 div="host-vans-title">This is the host Vans page</h1>
+            <div className="host-vans-list">
+                {
+                    vans.length > 0 ? (
+                        <section>
+                            {hostVansEls}
+                        </section>
+                    ) : (
+                        <h2>Loading...</h2>
+                    )
+                }
+            </div>
+        </section>
+
     )
 }
