9a554f6f504e520f8071f61176fb2700959b30e3
rudy35hernandez <rudy35hernandez@gmail.com>
2024-03-31 17:48:51 -0700

    added dollar sign to vans list

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,6 +1,11 @@
 import React from "react"
+import {useParams} from "react-router-dom"
 
 export default function HostVanDetail(){
+
+    const params = useParams()
+    const [van, vanDetails] = React.useState([])
+
     return (
         <h1>
             This is one of the vans!
--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -22,7 +22,7 @@ export default function HostVans(){
                     <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
                     <div className="host-van-info">
                         <h3>{van.name}</h3>
-                        <p>{van.price}/day</p>
+                        <p>${van.price}/day</p>
                     </div>
                 </div>
             </Link>
