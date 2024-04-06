005b832a307122c3c769296cc30afa85e918cab6
rudy35hernandez <rudy35hernandez@gmail.com>
2024-04-06 00:08:50 -0700

    removed 0 index from object fetch

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -4,12 +4,12 @@ import {useParams, Link} from "react-router-dom"
 export default function HostVanDetail(){
 
     const params = useParams()
-    const [currentVan, setCurrentVan] = React.useState([])
+    const [currentVan, setCurrentVan] = React.useState(null)
 
     React.useEffect(() => {
         fetch(`/api/host/vans/${params.id}`)
             .then(res => res.json())
-            .then(data => setCurrentVan(data.vans[0]))
+            .then(data => setCurrentVan(data.vans))
     }, [])
 
     if(!currentVan) {
