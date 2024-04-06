85dab52a7fb209bcfe160ae2c60300c130b6e365
rudy35hernandez <rudy35hernandez@gmail.com>
2024-04-05 23:58:45 -0700

    added details for hostVanDetail in jsx for van

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -12,9 +12,31 @@ export default function HostVanDetail(){
             .then(data => setCurrentVan(data.vans))
     }, [])
 
-    return (
-        <>
+    if(!currentVan) {
+        return <h1>Loading...</h1>
+    }
 
-        </>
+    return (
+        <section>
+            <Link
+                to="?"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
+
+            <div className="host-van-detail-layout-container">
+                <div className="host-van-detail">
+                    <img src={currentVan.imageUrl} />
+                    <div className="host-van-detail-info-text">
+                        <i
+                            className={`van-type van-type-${currentVan.type}`}
+                        >
+                            {currentVan.type}
+                        </i>
+                        <h3>{currentVan.name}</h3>
+                        <h4>${currentVan.price}/day</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
