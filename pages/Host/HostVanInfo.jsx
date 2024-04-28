37144408fe7db8be8a4661722dfa35dcdd92e660
rudy35hernandez <rudy35hernandez@gmail.com>
2024-04-28 14:33:52 -0700

    added styling to host van info detail

--- pages/Host/HostVanInfo.jsx
+++ pages/Host/HostVanInfo.jsx
@@ -8,10 +8,10 @@ export default function HostVanInfo(){
     console.log(currentVan)
     
     return(
-        <>
+        <section className="host-van-detail-info">
             <h4>Name: <span>{currentVan.name}</span></h4>
             <h4>Category: <span>{currentVan.type}</span></h4>
             <h4>Description: <span>{currentVan.description}</span> </h4>
-        </>
+        </section>
     )
 }
