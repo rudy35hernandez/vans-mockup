639ce5016f1c343ed33c30ab0f36d2de433b09a0
rudy35hernandez <rudy35hernandez@gmail.com>
2024-04-28 13:49:02 -0700

    added outlet context to hostvaninfo

--- pages/Host/HostVanInfo.jsx
+++ pages/Host/HostVanInfo.jsx
@@ -1,11 +1,13 @@
 import React from "react"
+import {useOutletContext} from "react-router-dom"
 
 export default function HostVanInfo(){
 
+    const [currentVan, setCurrentVan] = useOutletContext()
 
     return(
         <>
-            <h1>Test</h1>
+            <h1>{currentVan.name}</h1>
         </>
     )
 }
