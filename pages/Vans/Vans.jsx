80bc601a65c88a7e88c89b2c4fb78562f5e1c3f5
rudy35hernandez <rudy35hernandez@gmail.com>
2024-05-15 14:15:10 -0700

    added searchParams to filter vans by type

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,11 +1,14 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import {Link, useSearchParams} from "react-router-dom"
 
 export default function Vans(){
     const [vans, setVans] = React.useState([])
 
-  
+    const [searchParams, setSearchParams] = useSearchParams()
 
+    const typeFilter = searchParams.get("type")
+
+    const vansToDisplay = typeFilter ? vans.filter(van => van.type === typeFilter) : vans
 
     React.useEffect(() => {
         fetch("/api/vans")
@@ -13,7 +16,7 @@ export default function Vans(){
             .then(data => setVans(data.vans))
     }, [])
 
-    const vansEl = vans.map(el => {
+    const vansEl = vansToDisplay.map(el => {
         return (
             <div key={el.id} className="van-tile">
                 <Link to={`/vans/${el.id}`}>
