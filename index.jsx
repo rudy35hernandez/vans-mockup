14e57ce762b0a316cb269031ac62f9ffe3403a03
rudy35hernandez <rudy35hernandez@gmail.com>
2024-05-15 14:12:26 -0700

    fixed directory

--- index.jsx
+++ index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans/Vans.jsx"
+import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Reviews from "./pages/Host/Reviews"
