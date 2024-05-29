b29ad00e17f00452cc6164bafbcfd65755241092
rudy35hernandez <rudy35hernandez@gmail.com>
2024-05-28 23:01:05 -0700

    used setSearchParams to filter vans

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -35,18 +35,25 @@ export default function Vans(){
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
-                <Link to="?type=rugged" className="van-type rugged">
+                <button onClick={() => setSearchParams({type: "rugged"})}
+                className="van-type rugged">
                     Rugged
-                </Link>
-                <Link to="?type=luxury" className="van-type luxury">
+                </button>
+                <button
+                    onClick={() => setSearchParams({type: "luxury"})}
+                    className="van-type luxury">
                     Luxury
-                </Link>
-                <Link to="?type=simple" className="van-type simple">
+                </button>
+                <button 
+                    onClick={() => setSearchParams({type: "simple"})}
+                    className="van-type simple">
                     Simple
-                </Link>
-                <Link to="." className="van-type clear-filter">
+                </button>
+                <button
+                    onClick={() => setSearchParams({})}
+                    className="van-type clear-filter">
                     Clear
-                </Link>
+                </button>
             </div>
             <div className="van-selections">
                 {vansEl}
