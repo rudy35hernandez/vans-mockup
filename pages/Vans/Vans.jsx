a0ba11b58d20923b21d88a21014c1014a4f67354
rudy35hernandez <rudy35hernandez@gmail.com>
2024-05-29 23:33:33 -0700

    testing fix for vans page

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -47,27 +47,31 @@ export default function Vans(){
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
-                <button
-                    onClick={() => handleFilterChange("type", "simple")}
-                    className="van-type simple"
-                >Simple</button>
+                <button 
+                    onClick={() => handleFilterChange("type", "rugged")}
+                    className="van-type rugged">
+                    Rugged
+                </button>
                 <button
                     onClick={() => handleFilterChange("type", "luxury")}
-                    className="van-type luxury"
-                >Luxury</button>
-                <button
-                    onClick={() => handleFilterChange("type", "rugged")}
-                    className="van-type rugged"
-                >Rugged</button>
+                    className="van-type luxury">
+                    Luxury
+                </button>
+                <button 
+                    onClick={() => handleFilterChange("type", "simple")}
+                    className="van-type simple">
+                    Simple
+                </button>
                 <button
                     onClick={() => handleFilterChange("type", null)}
-                    className="van-type clear-filters"
-                >Clear filter</button>
-
+                    className="van-type clear-filter">
+                    Clear
+                </button>
             </div>
-            <div className="van-list">
-                {vanElements}
+            <div className="van-selections">
+                {vansEl}
             </div>
         </div>
+        
     )
 }
