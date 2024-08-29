294f47ce5f9a15f546640c9d17c881e8cedef869
rudy35hernandez <rudy35hernandez@gmail.com>
2024-08-28 23:23:07 -0700

    added styling to link and catch all page

--- pages/NotFound.jsx
+++ pages/NotFound.jsx
@@ -3,9 +3,9 @@ import {Link} from "react-router-dom"
 
 export default function NotFound(){
     return (
-        <>
+        <div className="catch-all">
             <h2>Sorry, the page you were looking for was not found.</h2>
-            <Link>Return to home</Link>
-        </>
+            <Link to="/">Return to home</Link>
+        </div>
     )
 }
