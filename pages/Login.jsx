c161fa8f9efaaff9d2fabdf6ab26a9833a7e57dd
rudy35hernandez <rudy35hernandez@gmail.com>
2024-09-08 15:20:52 -0700

    added console log of loginformdata to test input event

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -5,11 +5,17 @@ export default function Login(){
     const [loginFormData, setLoginFormData] = React.useState({email: "", password: ""})
 
     function handleSubmit(e){
-
+        e.preventDefault()
+        console.log(loginFormData)
     }
 
     function handleChange(e){
-        
+        const { name, value } = e.target
+        setLoginFormData( prev => ({
+            ...prev,
+            [name]: value
+            })
+        )   
     }
 
     return (
