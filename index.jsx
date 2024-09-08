699e451c38906f9e65845f76d5fb500b3aacc823
rudy35hernandez <rudy35hernandez@gmail.com>
2024-09-08 16:00:41 -0700

    fixed type in login route

--- index.jsx
+++ index.jsx
@@ -29,7 +29,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="login" element={Login />} />
+          <Route path="login" element={<Login />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
 
