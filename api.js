27a71f55c73f114af08cae95f686599d06e7e25f
rudy35hernandez <rudy35hernandez@gmail.com>
2024-10-12 00:17:14 -0700

    updated api js to handle incorrect login errors

--- api.js
+++ api.js
@@ -1,6 +1,11 @@
-export async function getVans(){
-    const res = await fetch("/api/vans")
-    if(!res.ok){  // res.ok is built into fetch. If it is NOT ok, it will throw a 400 or 500 level response code
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(() => resolve(), ms))
+}
+
+export async function getVans(id) {
+    const url = id ? `/api/vans/${id}` : "/api/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
         throw {
             message: "Failed to fetch vans",
             statusText: res.statusText,
@@ -9,4 +14,35 @@ export async function getVans(){
     }
     const data = await res.json()
     return data.vans
+}
+
+export async function getHostVans(id) {
+    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
+}
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
 }
--- components/AuthRequired.jsx
+++ components/AuthRequired.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import {Navigate, Outlet} from "react-router-dom"
 
 export default function AuthRequired(){
-    const authenticated = true
+    const authenticated = false
 
     if(!authenticated){
         return <Navigate to="/Login" state={{message: "You must login first"}}/>
