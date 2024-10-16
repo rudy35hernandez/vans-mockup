130a8d50bc737f6055fefecd3133ee57bc9f93da
rudy35hernandez <rudy35hernandez@gmail.com>
2024-10-15 22:42:52 -0700

    added visibility conditions based on login

--- pages/Host/HostVanInfo.jsx
+++ pages/Host/HostVanInfo.jsx
@@ -5,13 +5,14 @@ export default function HostVanInfo(){
 
     const [currentVan, setCurrentVan] = useOutletContext()
 
-    console.log(currentVan)
+    // console.log(currentVan)
     
     return(
         <section className="host-van-detail-info">
             <h4>Name: <span>{currentVan.name}</span></h4>
             <h4>Category: <span>{currentVan.type}</span></h4>
             <h4>Description: <span>{currentVan.description}</span> </h4>
+            <h4>Visibility: <span>Public</span></h4>
         </section>
     )
 }
